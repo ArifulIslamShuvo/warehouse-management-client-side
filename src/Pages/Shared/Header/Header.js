a1fa67d269f3afa331f1333a27fe6bb102532340
Ariful Islam Shuvo <arifulislams063@gmail.com>
2022-05-04 21:31:10 +0600

    implementation signOut

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -3,26 +3,40 @@ import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import '../Header/Header.css';
 import { AiOutlineLogin } from "react-icons/ai";
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import { signOut } from 'firebase/auth';
 
 const Header = () => {
+    const [user, loading, error] = useAuthState(auth);
+//-----------handled sign out-----------//
+    const logout = () => {
+        signOut(auth);
+    };
+
     return (
         <div>
-             <Navbar className='navbar' collapseOnSelect expand="lg"  variant="dark">
+            <Navbar className='navbar' collapseOnSelect expand="lg" variant="dark">
                 <Container>
                     <Navbar.Brand as={Link} to="/" className='fs-3'>
                         <div className='mt-4'>
-                        <h2 className='real'>REAL</h2>
-                        <p className='furniture'>FURNITURE</p>
+                            <h2 className='real'>REAL</h2>
+                            <p className='furniture'>FURNITURE</p>
                         </div>
-                        </Navbar.Brand>
+                    </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="mx-auto">
                             <Nav.Link className='fs-5 text-white' as={Link} to="home"><span className='nav'>Home</span></Nav.Link>
                             <Nav.Link className='fs-5 text-white' as={Link} to="inventorys"><span className='nav'>Inventorys</span></Nav.Link>
                         </Nav>
-                        <Nav>
+                        <Nav>{
+                            !user?
                             <Nav.Link as={Link} to="login" className='fs-5 text-white'><span className='login'>Login <AiOutlineLogin /></span></Nav.Link>
+                            :
+                            <Nav.Link as={Link} to="login" className='fs-5 text-white'><span className='login' onClick={logout}>Logout <AiOutlineLogin /></span></Nav.Link>
+                            
+                        }
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
